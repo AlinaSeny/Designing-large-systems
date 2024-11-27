03715ecf21674c3569c415f489e12f1396437a45
meursault <meursault307@gmail.com>
2024-11-27 20:57:19 +0300

    fix exception

--- exception/exception.cpp
+++ exception/exception.cpp
@@ -5,42 +5,54 @@
 #include <gtest/gtest.h>
 
 class TMyException : public std::runtime_error {
+private:
+    std::string msg;
 public:
-    explicit TMyException(const std::string& msg) : std::runtime_error(msg) {}
+    TMyException() : std::runtime_error("My Exception") {}
 
-    friend std::ostream& operator<<(std::ostream& os, const TMyException& e) {
-        os << "TMyException: " << e.what();
-        return os;
+    const char* what() const noexcept override {
+        return msg.c_str();
+    }
+
+    template <typename T>
+    TMyException& operator<<(const T& value) {
+        std::ostringstream oss;
+        oss << msg << value;
+        msg = oss.str();
+        return *this;
     }
 };
 
-class TMyExceptionA : public TMyException {
-public:
-    explicit TMyExceptionA(const std::string& msg) : TMyException("A: " + msg) {}
-};
+class TMyExceptionA : public TMyException {};
 
-class TMyExceptionB : public TMyException {
-public:
-    explicit TMyExceptionB(const std::string& msg) : TMyException("B: " + msg) {}
-};
+class TMyExceptionB : public TMyException {};
+
+void ThrowMyException() {
+    try {
+        throw TMyException() << "TMyException";
+    } catch (const TMyException& exception) {
+        std::cout << exception.what() << std::endl;
+    }
+}
 
 void ThrowExceptionA() {
-    throw TMyExceptionA("This is an exception of type A");
+    throw TMyExceptionA() << "This is an exception of type A";
 }
 
 void ThrowExceptionB() {
-    throw TMyExceptionB("This is an exception of type B");
+    throw TMyExceptionB() << "This is an exception of type B";
 }
 
 TEST(MyExceptionTest, TestThrowExceptionA) {
-    EXPECT_THROW(ThrowExceptionA(), TMyExceptionA);
+    EXPECT_THROW(ThrowExceptionA(), TMyException);
 }
 
 TEST(MyExceptionTest, TestThrowExceptionB) {
-    EXPECT_THROW(ThrowExceptionB(), TMyExceptionB);
+    EXPECT_THROW(ThrowExceptionB(), TMyException);
 }
 
 int main(int argc, char** argv) {
+    ThrowMyException();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
